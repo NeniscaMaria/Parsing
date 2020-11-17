0207105394b0a79336d2dd4e738ba44e36709d03
Nenisca Maria <neniscamaria13@gmail.com>
2020-11-17 18:01:00 +0200

    more methods to tree class

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,13 @@ public class Main {
         tree.addChild("A","B");
         tree.addChild("A","E");
         tree.addChild("B","C");
+        tree.addChild("B","G");
         tree.addChild("C","D");
         tree.addChild("C","F");
         System.out.println(tree);
         System.out.println(tree.findParent(tree.getRoot(),"E").getVal());
-        System.out.println(tree.getKthChild(tree.getRoot(),"A",2));
+        System.out.println(tree.getKthChild(tree.getRoot(),"D",1));
+        System.out.println(tree.getLeaves(tree.getRoot()));
     }
 
     public static void main(String[] args) {
