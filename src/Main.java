428dffa7246b98b03d7e8c138e02e31b44242684
Nenisca Maria <neniscamaria13@gmail.com>
2020-11-17 19:27:20 +0200

    menu to show grammar

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     private static void treeCheck(){
@@ -14,11 +16,45 @@ public class Main {
         System.out.println(tree.getLeaves(tree.getRoot()));
     }
 
-
-
+    private static void showMenu(){
+        System.out.println("Choose one of the following:");
+        System.out.println("1.Show set of nonterminals");
+        System.out.println("2.Show set of terminals");
+        System.out.println("3.Show set of productions");
+        System.out.println("4.Show production for a nonterminal");
+        System.out.println("0.Exit");
+    }
     public static void main(String[] args) {
         treeCheck();
         Grammar grammar = new Grammar();
-        System.out.println(grammar);
+        boolean finsihed = false;
+        Scanner console = new Scanner(System.in);
+        while(!finsihed){
+            showMenu();
+            System.out.println(">>");
+            int choice = console.nextInt();
+            switch (choice){
+                case 0:
+                    finsihed=true;
+                    break;
+                case 1:
+                    System.out.println(grammar.getNonTerminals());
+                    break;
+                case 2:
+                    System.out.println(grammar.getTerminals());
+                    break;
+                case 3:
+                    System.out.println(grammar.getProductions());
+                    break;
+                case 4:
+                    System.out.println("Nonterminal: ");
+                    console.nextLine();
+                    String nonterminal = console.nextLine();
+                    System.out.println(grammar.getProductionsContainingNonterminal(nonterminal));
+                    break;
+                default:
+                    System.out.println("Wrong command");
+            }
+        }
     }
 }
