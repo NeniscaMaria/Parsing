e6f08d4245185821da61fdc20d13328abc9da5b6
Nenisca Maria <neniscamaria13@gmail.com>
2020-11-17 17:40:39 +0200

    first draft of tree

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
+    private static void treeCheck(){
+        Tree tree = new Tree(new Node("A"));
+        tree.addChild("A","B");
+        tree.addChild("A","E");
+        tree.addChild("B","C");
+        tree.addChild("C","D");
+        tree.addChild("C","F");
+        System.out.println(tree);
+        System.out.println(tree.findParent(tree.getRoot(),"E").getVal());
+        System.out.println(tree.getKthChild(tree.getRoot(),"A",2));
+    }
+
     public static void main(String[] args) {
-        
+        treeCheck();
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,107 @@
 public class Tree {
+    private Node root;
+
+    public Tree(Node root) {
+        this.root = root;
+    }
+
+    public Node addChild(String parent, String child){
+        //returns the added node
+        Node node = findNode(root, parent);
+        Node toAdd = new Node(child);
+        if(node!=null){
+            if(node.getChild()==null) {
+                node.setChild(toAdd);
+            }else{
+                Node next = findNextFree(node.getChild());
+                next.setNext(toAdd);
+            }
+            return toAdd;
+        }
+        return null;
+    }
+
+    public Node addChildToNode(Node parent, String child){
+        //returns the added node
+        Node nodeToAdd = new Node(child);
+        if(parent!=null){
+            if(parent.getChild()==null) {
+                parent.setChild(nodeToAdd);
+            }else{
+                Node next = findNextFree(parent.getChild());
+                next.setNext(nodeToAdd);
+            }
+            return nodeToAdd;
+        }
+        return null;
+    }
+
+    private Node findNextFree(Node node) {
+        if (node.getNext() == null)
+            return node;
+        else return findNextFree(node.getNext());
+    }
+
+    public Node findNode(Node current, String value){
+        if (current == null) return null;
+        if(current.getVal().equals(value))
+            return current;
+        else {
+            Node candidate = findNode(current.getChild(), value);
+            if(candidate == null)
+                return findNode(current.getNext(), value);
+            else
+                return candidate;
+        }
+    }
+
+    public Node findParent(Node current, String node){
+        if(current == null)
+            return null;
+        if(current.getChild().getVal().equals(node))
+            return current;
+        if(searchSiblings(current.getChild(),node))
+            return current;
+        else{
+            return findParent(current.getChild(),node);
+        }
+    }
+
+    private boolean searchSiblings(Node current, String value){
+        if(current==null) return false;
+        if(current.getVal().equals(value))
+            return true;
+        else return searchSiblings(current.getNext(),value);
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public Node getKthChild(Node root, String P, int childNo) {
+        if (root == null)
+            return null;
+        if (root.getVal().equals(P)) {
+            Node t = root.getChild();
+            int i = 1;
+            while (t != null && i < childNo) {
+                t = t.getNext();
+                i++;
+            }
+            if (t == null)
+                return null;
+            else
+                return(t);
+        }
+        getKthChild(root.getChild(), P, childNo);
+        getKthChild(root.getNext(), P, childNo);
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Tree{" +
+                "root=" + root +
+                '}';
+    }
 }
